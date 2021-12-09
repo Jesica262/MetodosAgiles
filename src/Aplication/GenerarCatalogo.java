65e333adbd2a6a05b521e5b71ffc1652edb25f3d
Jesica262 <yesiifer@hotmail.com>
2021-12-09 11:22:22 -0300

    09/11

--- src/Aplication/GenerarCatalogo.java
+++ src/Aplication/GenerarCatalogo.java
@@ -25,24 +25,23 @@ import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class GenerarCatalogo extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textNumero;
-	private JTextField textProvincia;
-	private JTextField textLocalidad;
-	private JTextField textCodigo;
-	private JTextField textCalle;
-	private JTextField textTipo;
-	private JTextField textPrecio;
+	private JTable table;
 	
 	public GenerarCatalogo() {
 		setBackground(SystemColor.inactiveCaption);
-		setTitle("Generar Reserva");
+		setTitle("Generar Catalogo");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 650);
+		setBounds(100, 100, 600, 686);
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.inactiveCaption);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -52,10 +51,10 @@ public class GenerarCatalogo extends JFrame {
 		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_1.setBackground(SystemColor.activeCaption);
 		
-		JLabel lblPropietarioX = new JLabel("RESERVAR INMUEBLE");
+		JLabel lblPropietarioX = new JLabel("LISTA DE CATALOGOS");
 		lblPropietarioX.setFont(new Font("Tahoma", Font.BOLD, 15));
 		
-		JButton btnAgregar = new JButton("Generar Reserva");
+		JButton btnAgregar = new JButton("Generar Catalogo");
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -77,273 +76,123 @@ public class GenerarCatalogo extends JFrame {
 		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_2.setBackground(SystemColor.inactiveCaption);
 		
-		JLabel lblNewLabel_2 = new JLabel("Seleccione Cliente:");
+		JLabel lblNewLabel_2 = new JLabel("Seleccione Inmuebles de la Lista:");
 		
-		JComboBox comboCliente = new JComboBox();
+		JPanel panel = new JPanel();
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+				.addGroup(gl_panel_2.createSequentialGroup()
 					.addGap(23)
-					.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(41, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
-					.addContainerGap(22, Short.MAX_VALUE)
-					.addComponent(comboCliente, GroupLayout.PREFERRED_SIZE, 218, GroupLayout.PREFERRED_SIZE)
-					.addGap(22))
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 230, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 499, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(24, Short.MAX_VALUE))
 		);
 		gl_panel_2.setVerticalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2.createSequentialGroup()
-					.addGap(20)
+				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+					.addContainerGap(31, Short.MAX_VALUE)
 					.addComponent(lblNewLabel_2)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(comboCliente, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(380, Short.MAX_VALUE))
+					.addGap(18)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 416, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 499, Short.MAX_VALUE)
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 416, Short.MAX_VALUE)
+		);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"Tipo de Inmueble", "Localidad", "Direcci\u00F3n", "Barrio", "Dormitorios", "Ba\u00F1os", "Garaje", "Patio", "Sup. Terrena", "Sup. Edificada", "Precio"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(103);
+		table.getColumnModel().getColumn(8).setPreferredWidth(89);
+		table.getColumnModel().getColumn(9).setPreferredWidth(97);
+		panel.setLayout(gl_panel);
 		panel_2.setLayout(gl_panel_2);
-		
-		JPanel panel_2_1 = new JPanel();
-		panel_2_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_2_1.setBackground(SystemColor.inactiveCaption);
-		
-		JLabel lblNewLabel_1_1_1_2 = new JLabel("Calle:");
-		
-		JLabel lblNewLabel_1_1 = new JLabel("C\u00F3digo:");
-		
-		JLabel lblNewLabel_1_1_1_2_1 = new JLabel("N\u00FAmero de Calle:");
-		
-		textNumero = new JTextField();
-		textNumero.setEditable(false);
-		textNumero.setColumns(10);
-		
-		JLabel lblNewLabel_1_1_1_2_1_1 = new JLabel("Provincia:");
-		
-		textProvincia = new JTextField();
-		textProvincia.setEditable(false);
-		textProvincia.setColumns(10);
-		
-		JLabel lblNewLabel_1_1_1_2_1_1_1 = new JLabel("Caracteristicas:");
-		
-		JLabel lblNewLabel_1_1_1_2_1_1_2 = new JLabel("Localidad:");
-		
-		textLocalidad = new JTextField();
-		textLocalidad.setEditable(false);
-		textLocalidad.setColumns(10);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("Datos del Inmueble Buscado");
-		
-		textCodigo = new JTextField();
-		textCodigo.setEditable(false);
-		textCodigo.setColumns(10);
-		
-		textCalle = new JTextField();
-		textCalle.setEditable(false);
-		textCalle.setColumns(10);
-		
-		JLabel lblTipo = new JLabel("Tipo de Inmueble:");
-		
-		textTipo = new JTextField();
-		textTipo.setEditable(false);
-		textTipo.setColumns(10);
-		
-		JLabel lblNewLabel_1_1_1_2_1_1_2_1 = new JLabel("Precio de Venta:");
-		
-		textPrecio = new JTextField();
-		textPrecio.setEditable(false);
-		textPrecio.setColumns(10);
-		GroupLayout gl_panel_2_1 = new GroupLayout(panel_2_1);
-		gl_panel_2_1.setHorizontalGroup(
-			gl_panel_2_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2_1.createSequentialGroup()
-					.addGroup(gl_panel_2_1.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_2_1.createSequentialGroup()
-							.addGap(33)
-							.addGroup(gl_panel_2_1.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(lblNewLabel_1_1_1_2_1_1_1, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblNewLabel_1_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textCodigo, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 195, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblNewLabel_1_1_1_2, Alignment.LEADING)
-								.addComponent(textCalle, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 195, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblNewLabel_1_1_1_2_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textNumero, Alignment.LEADING)
-								.addComponent(lblNewLabel_1_1_1_2_1_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textProvincia, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
-								.addComponent(lblNewLabel_1_1_1_2_1_1_2, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textLocalidad, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
-								.addComponent(lblTipo, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textTipo, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 195, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblNewLabel_1_1_1_2_1_1_2_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textPrecio, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 195, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_panel_2_1.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblNewLabel_2_1, GroupLayout.PREFERRED_SIZE, 196, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(34, Short.MAX_VALUE))
-		);
-		gl_panel_2_1.setVerticalGroup(
-			gl_panel_2_1.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_panel_2_1.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel_2_1)
-					.addGap(18)
-					.addComponent(lblNewLabel_1_1)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textCodigo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblNewLabel_1_1_1_2)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textCalle, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblNewLabel_1_1_1_2_1)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textNumero, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(11)
-					.addComponent(lblNewLabel_1_1_1_2_1_1)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textProvincia, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(11)
-					.addComponent(lblNewLabel_1_1_1_2_1_1_2)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textLocalidad, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblTipo)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(textTipo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblNewLabel_1_1_1_2_1_1_2_1)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textPrecio, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(1015)
-					.addComponent(lblNewLabel_1_1_1_2_1_1_1)
-					.addGap(31))
-		);
-		panel_2_1.setLayout(gl_panel_2_1);
-		
-		JPanel panel_2_2 = new JPanel();
-		panel_2_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_2_2.setBackground(SystemColor.inactiveCaption);
-		
-		JLabel lblNewLabel_2_2 = new JLabel("Estado de Reserva:");
-		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Inmueble reservado");
-		chckbxNewCheckBox.setBackground(SystemColor.inactiveCaption);
-		chckbxNewCheckBox.setSelected(true);
-		GroupLayout gl_panel_2_2 = new GroupLayout(panel_2_2);
-		gl_panel_2_2.setHorizontalGroup(
-			gl_panel_2_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2_2.createSequentialGroup()
-					.addGap(23)
-					.addGroup(gl_panel_2_2.createParallelGroup(Alignment.LEADING)
-						.addComponent(chckbxNewCheckBox, GroupLayout.PREFERRED_SIZE, 220, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_2_2, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(19, Short.MAX_VALUE))
-		);
-		gl_panel_2_2.setVerticalGroup(
-			gl_panel_2_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2_2.createSequentialGroup()
-					.addGap(20)
-					.addComponent(lblNewLabel_2_2)
-					.addGap(18)
-					.addComponent(chckbxNewCheckBox)
-					.addContainerGap(30, Short.MAX_VALUE))
-		);
-		panel_2_2.setLayout(gl_panel_2_2);
-		
-		JPanel panel_2_2_1 = new JPanel();
-		panel_2_2_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_2_2_1.setBackground(SystemColor.inactiveCaption);
-		
-		JLabel lblNewLabel_2_2_1 = new JLabel("Ingreso de Datos para Reserva");
-		
-		JLabel lblNewLabel = new JLabel("Ingrese Importe:");
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel_3 = new JLabel("Seleccione dias:");
-		
-		JSpinner spinner = new JSpinner();
-		GroupLayout gl_panel_2_2_1 = new GroupLayout(panel_2_2_1);
-		gl_panel_2_2_1.setHorizontalGroup(
-			gl_panel_2_2_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2_2_1.createSequentialGroup()
-					.addGap(23)
-					.addComponent(lblNewLabel_2_2_1, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE))
-				.addGroup(gl_panel_2_2_1.createSequentialGroup()
-					.addGap(35)
-					.addGroup(gl_panel_2_2_1.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(spinner, Alignment.LEADING)
-						.addComponent(lblNewLabel_3, Alignment.LEADING)
-						.addComponent(lblNewLabel, Alignment.LEADING)
-						.addComponent(textField, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 165, Short.MAX_VALUE))
-					.addGap(62))
-		);
-		gl_panel_2_2_1.setVerticalGroup(
-			gl_panel_2_2_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2_2_1.createSequentialGroup()
-					.addGap(20)
-					.addComponent(lblNewLabel_2_2_1)
-					.addGap(18)
-					.addGap(17)
-					.addComponent(lblNewLabel)
-					.addGap(11)
-					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblNewLabel_3)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(spinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(93, Short.MAX_VALUE))
-		);
-		panel_2_2_1.setLayout(gl_panel_2_2_1);
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(71)
-					.addComponent(lblPropietarioX, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(246, Short.MAX_VALUE))
-				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 550, GroupLayout.PREFERRED_SIZE)
 						.addGroup(gl_panel_1.createSequentialGroup()
-							.addContainerGap()
 							.addComponent(btnAgregar, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(11)
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
-								.addComponent(panel_2_2_1, GroupLayout.DEFAULT_SIZE, 274, Short.MAX_VALUE)
-								.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 274, Short.MAX_VALUE)
-									.addComponent(panel_2_2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(panel_2_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE)))
 					.addGap(258)
 					.addComponent(lblNewLabel_4, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 					.addGap(14))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(66)
+					.addComponent(lblPropietarioX, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(602, Short.MAX_VALUE))
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(24)
-					.addComponent(lblPropietarioX)
-					.addGap(11)
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING, false)
+					.addGap(43)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_4)
-								.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(panel_2_2, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(panel_2_2_1, GroupLayout.PREFERRED_SIZE, 215, GroupLayout.PREFERRED_SIZE))
-						.addComponent(panel_2_1, 0, 0, Short.MAX_VALUE))
-					.addPreferredGap(ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+							.addGap(11)
+							.addComponent(lblNewLabel_4))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addGap(37)
+							.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 476, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnCancelar)
 						.addComponent(btnAgregar))
-					.addGap(25))
+					.addGap(28))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(35)
+					.addComponent(lblPropietarioX)
+					.addContainerGap(579, Short.MAX_VALUE))
 		);
 		panel_1.setLayout(gl_panel_1);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
